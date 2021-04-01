233f3c3ec70067167e8f31601feaeca641e48a46
Adriano <adrian_george28@yahoo.com>
2021-04-01 17:34:47 +0100

    Add Dom Elements

--- modal.js
+++ modal.js
@@ -11,6 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("btn-submit");
+const confirmationMessage = document.getElementById("confirmation");
+const modalClose = document.querySelector(".close");
+
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const tournamentsAttended = document.getElementById("quantity");
+const radioButtons = document.querySelectorAll("[name='location']");
+const radioButtonsArray = Array.from(radioButtons);
+const termsAndCons = document.getElementById("checkbox1");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
