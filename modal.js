36a34f633079133a97e432b90e42ef69df54a3cf
Adriano <adrian_george28@yahoo.com>
2021-04-02 19:30:38 +0100

    Fix hambuger menu for responsive website

--- modal.js
+++ modal.js
@@ -112,6 +112,7 @@ function removeInvalidInputs() {
 	email.classList.remove("confirmSuccess");
 	birthDay.classList.remove("confirmSuccess");
 	tournaments.classList.remove("confirmSuccess");
+	errorFix.style.display="block";
 }
 
 // check the value for inputs
