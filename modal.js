5a92fc86b2936e7c99b99619438bb40fa85daa03
Adriano <adrian_george28@yahoo.com>
2021-04-02 10:18:46 +0100

    Add Form Listener and prevent default submit

--- modal.js
+++ modal.js
@@ -1,10 +1,12 @@
+/** @format */
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // DOM Elements
@@ -19,27 +21,52 @@ const modalClose = document.querySelector(".close");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
-const birthDate = document.getElementById("birthdate");
-const tournamentsAttended = document.getElementById("quantity");
+const birthDay = document.getElementById("birthdate");
+const tournaments = document.getElementById("quantity");
 const radioButtons = document.querySelectorAll("[name='location']");
 const radioButtonsArray = Array.from(radioButtons);
-const termsAndCons = document.getElementById("checkbox1");
-
+const termsAndConditions = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = "block";
 }
-
+//--Close modal form + reset after closing
+//__add this function on top of the listener
+//__to prevent access before initialization
+const closeButtonX = () => {
+	modalbg.style.display = "none";
+	form.reset();
+	console.log("closeButtonX=Click");
+	removeInvalidStyling();
+};
+// close modal event
 modalClose.addEventListener("click", closeButtonX);
 
-function closeButtonX() {
-  modalbg.style.display = "none";
-  form.reset();
-  console.log("closeButtonX");
+function removeInvalidStyling() {
+	formData.forEach((node) => node.removeAttribute("data-error-visible"));
+	formData.forEach((node) => node.removeAttribute("data-error"));
 }
+// prevent submit
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log("form event ");
+
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+  
